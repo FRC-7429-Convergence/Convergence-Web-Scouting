d3c03bd2969c720a51987ab2543179e654cb4912
Ethen <ebot14rocks@gmail.com>
2024-05-05 18:16:04 -0500

    started work on pit scouting UI

--- frontend/src/pages/Pit.js
+++ frontend/src/pages/Pit.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import './pit.css';
 
 function Pit() {
-  return (
-    <div>Pit</div>
-  )
+    return (
+        <>
+            <div className='title'>
+                <h2>Pit Scouting</h2>
+            </div>
+
+            <form action='submit'>
+                <div className='inputs'>
+                    <label>
+                        Team Number: <input name='teamNumber' defaultValue="Team #" />
+                    </label>
+                </div>
+
+                <div className='inputs'>
+                    <label>
+                        Drivetrain: {" "}
+                        <select name='drivetrain' id='drivetrain'>
+                            <option id='tank' value='tank'>Tank</option>
+                            <option id='swerve' value='swerve'>Swerve</option>
+                            <option id='mecanum' value='mecanum'>Mecanum</option>
+                        </select>
+                    </label>
+                </div>
+
+
+                <div className='submit'>
+                    <button type='submit' className='btn btn-success'>Save</button>
+                </div>
+            </form>
+        </>
+    )
 }
 
 export default Pit
